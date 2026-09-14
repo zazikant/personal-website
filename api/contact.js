5a3eafe5bc2dfec4945ba7fe6c8352b3bdd14b22
Shashikant Zarekar <zazikant@gmail.com>
2026-09-14 06:52:01 +0000

    Set sender email to news@gemengserv.net, update port default to 465

--- api/contact.js
+++ api/contact.js
@@ -15,7 +15,7 @@ const nodemailer = require('nodemailer');
 
 const SMTP_HOST = process.env.SMTP_HOST || 'mail.gemengserv.net';
 const SMTP_PORT = parseInt(process.env.SMTP_PORT || '465', 10);
-const SMTP_USER = process.env.SMTP_USER || 'shashikant';
+const SMTP_USER = process.env.SMTP_USER || 'news@gemengserv.net';
 const SMTP_PASS = process.env.SMTP_PASS; // REQUIRED
 const MAIL_TO   = process.env.MAIL_TO   || 'shashikant.zarekar@gemengserv.com';
 
